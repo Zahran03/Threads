aba70adcc470844dee05808598fa9d0051ddf5e9
Zahran03 <zalfarizi502@gmail.com>
2024-10-21 21:42:53 +0700

    Edit: Topbar  component

--- components/index.ts
+++ components/index.ts
@@ -1,8 +1,8 @@
 import Navbar from "./Navbar";
-import Footer from "./Footer";
-import TopBar from "./TopBar";
-import LeftSidebar from "./LeftSidebar";
-import RightSidebar from "./RightSidebar";
-import BottomBar from "./BottomBar";
+import Footer from "./shared/Footer";
+import TopBar from "./shared/TopBar";
+import LeftSidebar from "./shared/LeftSidebar";
+import RightSidebar from "./shared/RightSidebar";
+import BottomBar from "./shared/BottomBar";
 
 export { Navbar, Footer, TopBar, LeftSidebar, RightSidebar, BottomBar };
